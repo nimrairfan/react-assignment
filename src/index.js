17693ba50f4b5913f41b273afe909943e273c375
Nimra Irfan <90278374+nimrairfan@users.noreply.github.com>
2022-02-01 02:59:07 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+
+ReactDOM.render(
+  <><h1><u><i>Hello! I'm learning React.</i></u></h1>
+  <br />
+  <h2><u><i>Name: Nimra Irfan</i></u></h2>
+  <h2><u><i>Qualification: 2nd year</i></u></h2>
+  <h2><u><i>Course: Web & Mob Development</i></u></h2>
+  <h2><u><i>Section: Morning 9 to 12</i></u></h2>
+  <h2><u><i>Batch: 4</i></u></h2>
+  <h2><u><i>Email: nimrairfan43320@gmail.com</i></u></h2>
+  <h2><u><i>From: Karachi Pakistan</i></u></h2>
+  <h2><u><i>Date: 31/1/2022</i></u></h2>
+</>,
+  document.getElementById("root")
+)
